e16782be5335d2b7b3305d124d10b0f7fe0c253d
nickbelwin <belwalkarsarvesh@gmail.com>
2023-12-04 20:11:49 +0530

    new

--- script.js
+++ script.js
@@ -55,23 +55,25 @@ links2.forEach((val) => {
         outercursor.classList.remove("groww");
     });
 });
+
+// srollbar custom=================================
+// setTimeout(()=>{
+    
+// },3000)
 let header;
 let myName;
 let sticky;
 let progress;
 let totalHeight;
-// srollbar custom=================================
-setTimeout(()=>{
-    header = document.getElementById("head");
-    myName = document.querySelector(".myName");
-    sticky = header.offsetTop;
-    progress = document.getElementById("progressBar");
-    totalHeight = document.body.scrollHeight - window.innerHeight;
-},3000)
+header = document.getElementById("head");
+myName = document.querySelector(".myName");
+sticky = header.offsetTop;
+progress = document.getElementById("progressBar");
+totalHeight = document.body.scrollHeight - window.innerHeight;
 window.onscroll = function () {
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     console.log("progress: ", progressHeight);
-    progress.style.height = progressHeight+11+ "%";
+    progress.style.height = progressHeight+ "%";
     if (window.pageYOffset > sticky) {
         header.classList.add("sticky");
         myName.innerHTML = ``;
@@ -89,8 +91,9 @@ dropMenu.addEventListener("click", (e) => {
     e.stopPropagation();
     if(flagdrop===true){
         flagdrop=false;
-        dropMenu.innerHTML="Back";
         document.querySelector(".dropmenu").style.transform="translateX(0px)";
+        dropMenu.innerHTML="Back";
+
         
     }
     else{
@@ -323,7 +326,7 @@ function pageAnimation() {
             ease: Expo.easeInOut
         })
 }
-// pageAnimation();
+pageAnimation();
 document.querySelectorAll(".projectelements").forEach(function (elem) {
     var rotate = 0;
     var diffrot = 0;
@@ -428,8 +431,22 @@ document.getElementById("page6").addEventListener("click", () => {
 //       message => alert(message)
 //     );
 // }
-let userName= document.getElementById("name").value;
 
+let page5= document.getElementById("page5");
+page5.addEventListener("mouseover", ()=>{
+    pluse1.style.transform="rotate(0deg)";
+    pluse2.style.transform="rotate(0deg)";
+    pluse3.style.transform="rotate(0deg)";
+    pluse4.style.transform="rotate(0deg)";
+    pluse5.style.transform="rotate(0deg)";
+    projectPage1.style.transform="translateX(-100rem)";
+    projectPage2.style.transform="translateX(-100rem)";
+    projectPage3.style.transform="translateX(-100rem)";
+    projectPage4.style.transform="translateX(-100rem)";
+    projectPage5.style.transform="translateX(-100rem)";
+})
+
+let userName= document.getElementById("name").value;
 
 function showContactError(){
   
@@ -472,6 +489,8 @@ function checkInput(e){
 }
 function isMailSent(){
     document.getElementById("notification").style.zIndex="1";
+    document.getElementById("notification").style.height=100+"vh";
+    document.getElementById("notification").style.width=100+"vw";
     document.getElementById("success").style.transform="scale(1)";
     document.getElementById("success").style.zIndex="1";
     
@@ -483,6 +502,8 @@ function successfullySent(){
         document.getElementById("name").value="";
         document.getElementById("email").value="";
         document.getElementById("message").value="";
+        document.getElementById("notification").style.height=0+"vh";
+        document.getElementById("notification").style.width=0+"vw";
         document.getElementById("notification").style.zIndex="-1";
     },500);
 }
@@ -542,13 +563,151 @@ function sendEmail(){
     
 }
 
-document.getElementById("mainLoader").style.transform="scale(1)";
-setTimeout(()=>{
-    document.getElementById("mainLoader").style.transform="scale(0)";
-    document.getElementById("main").style.display="block";
-    pageAnimation();
-},2300)
-setTimeout(()=>{
-    document.getElementById("mainLoader").style.display="none";
-    document.getElementById("socialbar").style.display="block";
-},3000)
+// starting loading screen----------------------------------------
+
+// document.getElementById("mainLoader").style.transform="scale(1)";
+// setTimeout(()=>{
+//     document.getElementById("mainLoader").style.transform="scale(0)";
+//     // document.getElementById("main").style.display="block";
+//     pageAnimation();
+// },2300)
+// setTimeout(()=>{
+//     document.getElementById("mainLoader").style.display="none";
+//     document.getElementById("socialbar").style.display="block";
+// },3000)
+let rotateFlag1=false;
+let rotateFlag2=false;
+let rotateFlag3=false;
+let rotateFlag4=false;
+let rotateFlag5=false;
+let pluse1=document.getElementById("rotatePluse1");
+let pluse2=document.getElementById("rotatePluse2");
+let pluse3=document.getElementById("rotatePluse3");
+let pluse4=document.getElementById("rotatePluse4");
+let pluse5=document.getElementById("rotatePluse5");
+let projectPage1=document.getElementById("tictactoeInfo");
+let projectPage2=document.getElementById("typingGameInfo")
+let projectPage3=document.getElementById("taskManagerInfo")
+let projectPage4=document.getElementById("movieDeckInfo")
+let projectPage5=document.getElementById("logPage");
+
+function rotate5(){
+    if(rotateFlag5===false){
+        rotateFlag5=true;
+        pluse5.style.transform="rotate(135deg)";
+        projectPage5.style.transform="translateX(0rem)";
+        pluse1.style.transform="rotate(0deg)";
+        pluse2.style.transform="rotate(0deg)";
+        pluse3.style.transform="rotate(0deg)";
+        pluse4.style.transform="rotate(0deg)";
+        projectPage1.style.transform="translateX(-100rem)";
+        projectPage2.style.transform="translateX(-100rem)";
+        projectPage3.style.transform="translateX(-100rem)";
+        projectPage4.style.transform="translateX(-100rem)";
+        rotateFlag1=false;
+        rotateFlag2=false;
+        rotateFlag3=false;
+        rotateFlag4=false;
+    }
+    else{
+        rotateFlag5=false
+        pluse5.style.transform="rotate(0deg)";
+        projectPage5.style.transform="translateX(-100rem)";
+    }
+}
+function rotate1(){
+    if(rotateFlag1===false){
+        rotateFlag1=true;
+        pluse1.style.transform="rotate(135deg)";
+        projectPage1.style.transform="translateX(0rem)";
+        pluse2.style.transform="rotate(0deg)";
+        pluse3.style.transform="rotate(0deg)";
+        pluse4.style.transform="rotate(0deg)";
+        pluse5.style.transform="rotate(0deg)";
+        projectPage2.style.transform="translateX(-100rem)";
+        projectPage3.style.transform="translateX(-100rem)";
+        projectPage4.style.transform="translateX(-100rem)";
+        projectPage5.style.transform="translateX(-100rem)";
+        rotateFlag2=false;
+        rotateFlag3=false;
+        rotateFlag4=false;
+        rotateFlag5=false;
+    }
+    else{
+        rotateFlag1=false
+        pluse1.style.transform="rotate(0deg)";
+        projectPage1.style.transform="translateX(-100rem)";
+    }
+}
+function rotate2(){
+    if(rotateFlag2===false){
+        rotateFlag2=true;
+        pluse2.style.transform="rotate(135deg)";
+        projectPage2.style.transform="translateX(0rem)";
+        pluse1.style.transform="rotate(0deg)";
+        pluse3.style.transform="rotate(0deg)";
+        pluse4.style.transform="rotate(0deg)";
+        pluse5.style.transform="rotate(0deg)";
+        projectPage1.style.transform="translateX(-100rem)";
+        projectPage3.style.transform="translateX(-100rem)";
+        projectPage4.style.transform="translateX(-100rem)";
+        projectPage4.style.transform="translateX(-100rem)";
+        rotateFlag1=false;
+        rotateFlag3=false;
+        rotateFlag4=false;
+        rotateFlag5=false;
+    }
+    else{
+        rotateFlag2=false
+        pluse2.style.transform="rotate(0deg)";
+        projectPage2.style.transform="translateX(-100rem)";
+    }
+}
+function rotate3(){
+    if(rotateFlag3===false){
+        rotateFlag3=true;
+        pluse3.style.transform="rotate(135deg)";
+        projectPage3.style.transform="translateX(0rem)";
+        pluse1.style.transform="rotate(0deg)";
+        pluse2.style.transform="rotate(0deg)";
+        pluse4.style.transform="rotate(0deg)";
+        pluse5.style.transform="rotate(0deg)";
+        projectPage1.style.transform="translateX(-100rem)";
+        projectPage2.style.transform="translateX(-100rem)";
+        projectPage4.style.transform="translateX(-100rem)";
+        projectPage5.style.transform="translateX(-100rem)";
+        rotateFlag1=false;
+        rotateFlag2=false;
+        rotateFlag4=false;
+        rotateFlag5=false;
+    }
+    else{
+        rotateFlag3=false
+        pluse3.style.transform="rotate(0deg)";
+        projectPage3.style.transform="translateX(-100rem)";
+    }
+}
+function rotate4(){
+    if(rotateFlag4===false){
+        rotateFlag4=true;
+        pluse4.style.transform="rotate(135deg)";
+        projectPage4.style.transform="translateX(0rem)";
+        pluse1.style.transform="rotate(0deg)";
+        pluse2.style.transform="rotate(0deg)";
+        pluse3.style.transform="rotate(0deg)";
+        pluse5.style.transform="rotate(0deg)";
+        projectPage1.style.transform="translateX(-100rem)";
+        projectPage2.style.transform="translateX(-100rem)";
+        projectPage3.style.transform="translateX(-100rem)";
+        projectPage5.style.transform="translateX(-100rem)";
+        rotateFlag1=false;
+        rotateFlag2=false;
+        rotateFlag3=false;
+        rotateFlag5=false;
+    }
+    else{
+        rotateFlag4=false
+        pluse4.style.transform="rotate(0deg)";
+        projectPage4.style.transform="translateX(-100rem)";
+    }
+}
